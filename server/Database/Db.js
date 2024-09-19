c656c94b62d3f5d05144784d0f742c8b53417ca0
WASEEM KHAN <waseem3khan333@gmail.com>
2024-09-19 22:33:55 +0500

    server error fix

--- server/Database/Db.js
+++ server/Database/Db.js
@@ -2,16 +2,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // database: process.env.DB_DATABASE
-    user: "postgres",
-    password: "1517",
-    host: "localhost",
-    port: "5432",
-    database: "postgres"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+
 })
 
 module.exports = pool;
